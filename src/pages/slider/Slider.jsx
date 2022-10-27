481dfdbf09ea68a4465cd9db4cded513de3d9285
Sergio Alvarez <sergioam2807@gmail.com>
2022-10-27 00:01:52 -0300

    ajustes en el slider

--- src/pages/slider/Slider.jsx
+++ src/pages/slider/Slider.jsx
@@ -31,7 +31,10 @@ export const Slider = () => {
             },
             500: {
               slidesPerView: 1,
-              width:500
+              width:400,
+              direction:"horizontal"
+              
+
             }}
           }
         >
@@ -40,19 +43,19 @@ export const Slider = () => {
             <img className='slider_image' src="https://i.ibb.co/VvzY9P0/Whats-App-Image-2022-10-23-at-11-08-20-AM.jpg" alt="Whats-App-Image-2022-10-23-at-11-08-20-AM" border="0" />
           </SwiperSlide>
 
-          <SwiperSlide> 
+          <SwiperSlide className="swiperslide">  
             <img className='slider_image' src="https://i.ibb.co/k0x7dfw/Whats-App-Image-2022-10-23-at-11-07-38-AM.jpg" alt="Whats-App-Image-2022-10-23-at-11-07-38-AM" border="0" />
           </SwiperSlide>
 
-          <SwiperSlide> 
+          <SwiperSlide className="swiperslide"> 
             <img className='slider_image' src="https://i.ibb.co/PwhqkWk/Whats-App-Image-2022-10-23-at-11-07-26-AM.jpg" alt="Whats-App-Image-2022-10-23-at-11-07-26-AM" border="0" />
           </SwiperSlide>
 
-          <SwiperSlide> 
+          <SwiperSlide className="swiperslide"> 
             <img className='slider_image' src="https://i.ibb.co/KwN4dBn/Whats-App-Image-2022-10-23-at-11-07-14-AM.jpg" alt="Whats-App-Image-2022-10-23-at-11-07-14-AM" border="0" />
           </SwiperSlide>
 
-          <SwiperSlide> 
+          <SwiperSlide className="swiperslide"> 
             <img className='slider_image' src="https://i.ibb.co/NCtxnPM/Whats-App-Image-2022-10-23-at-11-05-05-AM.jpg" alt="Whats-App-Image-2022-10-23-at-11-05-05-AM" border="0"/>
           </SwiperSlide>
         
