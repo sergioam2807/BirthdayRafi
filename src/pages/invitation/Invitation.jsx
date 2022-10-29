dda8c4aada3f7417a565d39a0546097981ccffd3
Sergio Alvarez <sergioam2807@gmail.com>
2022-10-29 10:06:49 -0300

    arreglo de imagen servidor

--- src/pages/invitation/Invitation.jsx
+++ src/pages/invitation/Invitation.jsx
@@ -7,7 +7,7 @@ export const Invitation = () => {
       <div className='invitation_container' id='invitation'>
 
           <div className='invitation_photo_container'>
-          <img className='invitation_image animate__animated animate__tada' src="https://i.ibb.co/27YygKM/raficontructor.jpg" alt="rafi" border="0" />
+          <img className='invitation_image animate__animated animate__tada' src="https://i.ibb.co/nfKWvWp/raficontructor.jpg" alt="rafi" border="0" />
           </div>
 
           <div className='invitation_text_container animate__animated animate__heartBeat'>
